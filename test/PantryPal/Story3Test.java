6322eb4810b42eb46bf87f3c113cf3c0e2d646e2
Thuan Do <thuan.do2003@yahoo.com>
2023-11-06 19:01:05 -0800

    Add 1 Unit Test
    Tried adding 1 unit test, which will see if pressing the button actually switches the scene to the correct one.

--- test/PantryPal/Story3Test.java
+++ test/PantryPal/Story3Test.java
@@ -1,4 +1,6 @@
 package PantryPal;
+
+// imports for unit tests
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -6,6 +8,55 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+// imports for Main.java
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.text.TextAlignment;
+import javafx.geometry.Insets;
+import javafx.scene.text.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.net.*;
+
 public class Story3Test {
-    
+    private static AppFrame appFrame;
+
+    @BeforeAll
+     public static void setUp() {
+        //declare og test recipe and start javafx
+        javafx.application.Platform.startup(() -> { });
+        appFrame = new AppFrame();
+     }
+
+     @AfterAll
+     public static void tearDown() {
+        //exit to prevent errors
+        javafx.application.Platform.exit();
+     }
+
+     @AfterEach
+     public void clearrecipes(){
+        //clear recipe list each time
+        appFrame.getRecipeList().getChildren().clear();
+     }
+
+     @Test 
+    // test to see whether the function linkd to the "New Recipe" button brings you to the Record MealType page
+    public void testAddButton() {
+        //this is what is linked to add recipe button
+        appFrame.handler.recordMeal();
+        // see whether we switched pages by 
+        assertEquals(appFrame.handler.primaryStage, pageList.get(RECORD_MEALTYPE));
+    }
 }
