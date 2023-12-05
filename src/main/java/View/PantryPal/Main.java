eebef3118ce32186a20a8b8cd5246200c875cee5
tylermeyers <88351548+tylermeyers@users.noreply.github.com>
2023-12-04 20:48:33 -0800

    Feature 4, minor fixes. #98,#99,#100,#101

--- src/main/java/View/PantryPal/Main.java
+++ src/main/java/View/PantryPal/Main.java
@@ -684,9 +684,8 @@ class GPTResultsDisplay extends BorderPane{
         });
         Button reGenButton = footer.getReGenButton();
         reGenButton.setOnAction(e->{
-             RequestHandler reqHandler = ((RecordAppFrame)this.nHandler.getMap().get("RecordIngredients").getRoot()).getRequestHandler();
+            RequestHandler reqHandler = ((RecordAppFrame)this.nHandler.getMap().get("RecordIngredients").getRoot()).getRequestHandler();
             Recipe Rrecipe = cHandler.getRecipe();
-            System.out.println( "MEALTYPE: " + Rrecipe.getMealType().toString() + " INGREDIENTS: " + Rrecipe.getIngredients());
             String recipe = reqHandler.performGenerateRequest("PUT", Rrecipe.getMealType().toString(), Rrecipe.getIngredients());
             //ScrollPane scrollPane2 = createScrollableBox("Instructions: "+ cHandler.getRecipe().getInstructions());
             Rrecipe.setInstructions(recipe);
